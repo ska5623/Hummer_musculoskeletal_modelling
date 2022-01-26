b135606d4113e83139f55c0e3bbb4e6e82618e69
ska5623 <98357116+ska5623@users.noreply.github.com>
2022-01-26 11:09:18 -0500

    Update load_data.m

--- Final Data and plotting/1. Load data/load_data.m
+++ Final Data and plotting/1. Load data/load_data.m
@@ -1,59 +1,59 @@
 clear
 load('data_elbow_wl_same_phase')
 linear_testing_elbow_wl_same_phase
-save('Results data\elbow_wl_same_linear.mat');
+save('Results data\elbow_wl_same_phase.mat');
 
 clear
 load('data_elbow_wl_sep_phase')
 linear_testing_elbow_wl_sep_phase
-save('Results data\elbow_wl_sep_linear.mat');
+save('Results data\elbow_wl_sep_phase.mat');
 
 clear
 load('data_elbow_wl_mh_same_phase.mat')
 linear_testing_elbow_wl_mh_same_phase
-save('Results data\elbow_mh_wl_linear.mat');
+save('Results data\elbow_mh_wl_same_phase.mat');
 
 clear
 load('data_elbow_wl_mh_sep_phase.mat')
 linear_testing_elbow_wl_mh_sep_phase
-save('Results data\elbow_mh_wl_linear_sep_phase.mat');
+save('Results data\elbow_mh_wl_sep_phase.mat');
 
 clear
 load('data_wrist_wl_same_phase')
 linear_testing_wrist_wl_same_phase
-save('Results data\wrist_wl_linear.mat');
+save('Results data\wrist_wl_same_phase.mat');
 
 clear
 load('data_wrist_wl_sep_phase')
 linear_testing_wrist_wl_sep_phase
-save('Results data\wrist_wl_linear_sep_phase.mat');
+save('Results data\wrist_wl_sep_phase.mat');
 
 clear
 load('data_wrist_wl_mh_same_phase')
 linear_testing_wrist_wl_mh_same_phase
-save('Results data\wrist_mh_wl_linear.mat');
+save('Results data\wrist_mh_wl_same_phase.mat');
 
 clear
 load('data_wrist_wl_mh_sep_phase')
 linear_testing_wrist_wl_mh_sep_phase
-save('Results data\wrist_mh_wl_linear_sep_phase.mat');
+save('Results data\wrist_mh_wl_sep_phase.mat');
 
 clear
 load('data_body_fixed_wrist_same_phase')
 linear_testing_body_fixed_wrist_same_phase
-save('Results data\body_fixed_wrist_same_linear.mat');
+save('Results data\body_fixed_wrist_same_phase.mat');
 
 clear
 load('data_body_fixed_wrist_sep_phase')
 linear_testing_body_fixed_wrist_sep_phase
-save('Results data\body_fixed_wrist_sep_linear.mat');
+save('Results data\body_fixed_wrist_sep_phase.mat');
 
 clear
 load('data_body_fixed_elbow_same_phase')
 linear_testing_body_fixed_elbow_same_phase
-save('Results data\body_fixed_elbow_same_linear.mat');
+save('Results data\body_fixed_elbow_same_phase.mat');
 
 clear
 load('data_body_fixed_elbow_sep')
 linear_testing_body_fixed_elbow_sep_phase
-save('Results data\body_fixed_elbow_sep_linear.mat');
+save('Results data\body_fixed_elbow_sep_phase.mat');
