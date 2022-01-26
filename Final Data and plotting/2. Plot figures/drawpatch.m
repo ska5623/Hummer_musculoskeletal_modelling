c39ba7743f94dd57cae33869382765507cf5e4a5
ska5623 <98357116+ska5623@users.noreply.github.com>
2022-01-26 17:08:38 -0500

    Delete Final Data and plotting directory

--- Final Data and plotting/2. Plot figures/drawpatch.m
+++
@@ -1,3 +0,0 @@
-function h = drawpatch(minl, maxl,c,a)
-  h = patch([1:100 100:-1:1],[minl flipud(maxl')'],c,'EdgeColor','none','FaceAlpha',a);
-end
--- Final Data and plotting/2. Plot figures/drawpatch99.m
+++
@@ -1,3 +0,0 @@
-function h = drawpatch99(minl, maxl,c,a)
-  h = patch([1:99 99:-1:1],[minl flipud(maxl')'],c,'EdgeColor','none','FaceAlpha',a);
-end
